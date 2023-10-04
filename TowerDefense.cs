97bdb52bbbe13da5cf7c65022f89cc75791784e3
Ethan Shaw <eashaw2@asu.edu>
2023-10-04 01:28:13 -0700

    Extracted common functionality to library

--- TowerDefense.cs
+++ TowerDefense.cs
@@ -1,74 +1,49 @@
-﻿using System;
+﻿using MGLib;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 new TowerDefense();
 
-internal sealed class TowerDefense : Game {
+internal sealed class TowerDefense : NotatoGame {
+	private struct Tower {
+		internal byte[] name;
+		internal Texture2D texture;
+		internal byte cost;
+	}
+
 	private enum Scene : byte {
 		Title,
 		Battle
 	}
 
-	private SpriteBatch spriteBatch;
 	private Texture2D frameTexture;
 	private Texture2D titleScreenTexture;
 	private Texture2D sceneTexture;
 	private Texture2D arrowUpTexture;
 	private Texture2D arrowDownTexture;
-	private Texture2D[] characters = new Texture2D[10];
-	private Texture2D[] towers = new Texture2D[0];
 
-	private float scaleFactor;
-	private Vector2 offset;
+	private readonly Tower[] towers = new Tower[] {
+		new Tower() { name = new byte[] { 53, 10, 29, 38, 17, 14, 15 }, texture = null, cost = 2 }, // Produces cheese
+		new Tower() { name = new byte[] { 53, 10, 29, 37, 10, 23, 13, 18, 29 }, texture = null, cost = 5 }, // Steals money from enemies, melee
+		new Tower() { name = new byte[] { 53, 10, 29, 36, 27, 12, 17, 14, 27 }, texture = null, cost = 4 }, // Shoots in a straight line
+		new Tower() { name = new byte[] { 53, 10, 29, 46, 23, 18, 16, 17, 29 }, texture = null, cost = 2 } // Blocks enemies
+	};
 
-	private byte timer = 0;
-	private byte money = 0;
-
+	private int timer = 0;
+	private byte cheese = 2;
 	private Scene scene = Scene.Title;
 
-	internal TowerDefense() {
-		int internalWidth = 160;
-		int internalHeight = 90;
+	internal TowerDefense() : base(160, 90) {}
 
-		int width = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
-		int height = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
-		scaleFactor = Math.Min(width / internalWidth, height / internalHeight);
-		offset = new Vector2((width - internalWidth * scaleFactor) / 2, (height - internalHeight * scaleFactor) / 2);
-
-		new GraphicsDeviceManager(this) {
-			PreferredBackBufferWidth = width,
-			PreferredBackBufferHeight = height,
-			IsFullScreen = true
-		};
-		Run();
-	}
-
-	protected override void Initialize() {
-		spriteBatch = new SpriteBatch(GraphicsDevice);
-
-		Content.RootDirectory = "Content";
+	protected override void InitializeMethod() {
 		frameTexture = Content.Load<Texture2D>("frame");
 		titleScreenTexture = Content.Load<Texture2D>("title");
-
-		characters[0] = Content.Load<Texture2D>("0");
-		characters[1] = Content.Load<Texture2D>("1");
-		characters[2] = Content.Load<Texture2D>("2");
-		characters[3] = Content.Load<Texture2D>("3");
-		characters[4] = Content.Load<Texture2D>("4");
-		characters[5] = Content.Load<Texture2D>("5");
-		characters[6] = Content.Load<Texture2D>("6");
-		characters[7] = Content.Load<Texture2D>("7");
-		characters[8] = Content.Load<Texture2D>("8");
-		characters[9] = Content.Load<Texture2D>("9");
-
 		arrowUpTexture = Content.Load<Texture2D>("arrow_up");
 		arrowDownTexture = Content.Load<Texture2D>("arrow_down");
 
 		sceneTexture = titleScreenTexture;
-
-		base.Initialize();
 	}
 
 	protected override void Update(GameTime gameTime) {
@@ -78,29 +53,26 @@ internal sealed class TowerDefense : Game {
 			sceneTexture = frameTexture;
 			scene = Scene.Battle;
 		} else if (scene == Scene.Battle) {
-			if (timer < 60) {
+			if (timer < 600) {
 				timer++;
 			} else {
 				timer = 0;
-				if (money < 99) {
-					money++;
+				if (cheese < 99) {
+					cheese++;
 				}
 			}
 		}
 	}
 
-	protected override void Draw(GameTime gameTime) {
-		spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointClamp);
-
-		spriteBatch.Draw(sceneTexture, offset, null, Color.White, 0, Vector2.Zero, scaleFactor, SpriteEffects.None, 0);
+	protected override void DrawMethod() {
+		Draw(sceneTexture, 0, 0);
 
 		if (scene == Scene.Battle) {
-			spriteBatch.Draw(characters[money / 10], new Vector2(7, 81) * scaleFactor + offset, null, Color.White, 0, Vector2.Zero, scaleFactor, SpriteEffects.None, 0);
-			spriteBatch.Draw(characters[money % 10], new Vector2(13, 81) * scaleFactor + offset, null, Color.White, 0, Vector2.Zero, scaleFactor, SpriteEffects.None, 0);
-			spriteBatch.Draw(arrowUpTexture, new Vector2(21, 81) * scaleFactor + offset, null, Color.White, 0, Vector2.Zero, scaleFactor, SpriteEffects.None, 0);
-			spriteBatch.Draw(arrowDownTexture, new Vector2(21, 86) * scaleFactor + offset, null, Color.White, 0, Vector2.Zero, scaleFactor, SpriteEffects.None, 0);
+			DrawString(new[] { (byte) (cheese / 10), (byte) (cheese % 10) }, 7, 81);
+			Draw(arrowUpTexture, 21, 81);
+			Draw(arrowDownTexture, 21, 86);
+			DrawString(towers[0].name, 27, 81);
+			DrawString(new[] { towers[0].cost }, 154, 81);
 		}
-
-		spriteBatch.End();
 	}
 }
